3cf2e8b4199b27f9a5bcc4f00de9e932a744ba44
Max <maxpolak97@gmail.com>
2022-03-17 16:34:44 +0100

    npz file almost

--- data_processing.py
+++ data_processing.py
@@ -16,22 +16,26 @@ h3ds = H3DS(path='h3ds_v0.2')
 views_configs = h3ds.default_views_configs(scene_id='0cd3f3c0bc34a287') # '3', '4', '8', '16' and '32'
 
 # Load data of given scene and view configuration
-mesh, images, masks, cameras = h3ds.load_scene(scene_id='0cd3f3c0bc34a287', views_config_id='3')
+_, images, masks, cameras = h3ds.load_scene(scene_id='0cd3f3c0bc34a287', views_config_id='8', normalized=False)
+
 
 folder = 'scan1'
 
 # Creates a directory to save the data
-os.mkdir(folder) 
+if not os.path.isdir(folder):
+    os.mkdir(folder) 
 
 # Creates a directory to save the data
-os.mkdir(folder + '/image') 
+if not os.path.isdir(folder + '/image'):
+    os.mkdir(folder + '/image') 
 
 # Save images
 for n, image in enumerate(images):
     image.save(folder + '/image/img_000' + str(n) + '.jpg')
     
 # Creates a directory to save the data
-os.mkdir(folder + '/mask') 
+if not os.path.isdir(folder + '/mask'):
+    os.mkdir(folder + '/mask') 
 
 # Save masks
 for n, mask in enumerate(masks):
---
+++ npz_viewer.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 13:04:01 2022
+
+@author: Maxpo
+"""
+
+import numpy as np
+
+# IDR format
+data = np.load('DTU/scan65/cameras.npz')
+
+# for keys in data.files:
+#     print(keys)
+    
+print(data['world_mat_0'])
+print(data['camera_mat_inv_0'])
+print(data['scale_mat_0'])
+
+# H3D-Net format
+data1 = np.load('h3ds_v0.2/0cd3f3c0bc34a287/cameras.npz')
+
+for keys in data1.files:
+    print(keys)
+
+print(data['world_mat_0'])
+#print(data['camera_mat_inv_0'])
+print(data['scale_mat_0'])
