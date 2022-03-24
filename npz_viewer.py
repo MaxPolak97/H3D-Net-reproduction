b8a434ad61431b369a217a90334d86a9ad060510
AlonDawe <alon.dawe@gmail.com>
2022-03-24 16:51:26 +0100

    smalll changes
    small changes

--- npz_viewer.py
+++ npz_viewer.py
@@ -9,21 +9,26 @@ import numpy as np
 
 # IDR format
 #data = np.load('IDR/data/DTU/scan65/cameras.npz')
-data = np.load('IDR/data/H3D/view3/scan6/cameras.npz')
+#data = np.load('IDR/data/H3D/view3/scan6/cameras.npz')
+#data = np.load('h3')
 
-for keys in data.files:
-    print(keys)
+#for keys in data.files:
+    #print(keys)
     
-print(data['world_mat_0'])
-print(data['camera_mat_inv_0'])
-print(data['scale_mat_0'])
+#print(data['world_mat_0'])
+#print(data['camera_mat_inv_0'])
+#print(data['scale_mat_0'])
 
 # H3D-Net format
-data1 = np.load('h3ds_v0.2/0cd3f3c0bc34a287/cameras.npz')
-
+data1 = np.load('h3ds_v0.2/3b5a2eb92a501d54/cameras.npz')
+#print(data1.files)
 for keys in data1.files:
     print(keys)
 
-print(data['world_mat_0'])
+data2 = np.load('OWN_DATA/view3/scan6/cameras.npz')
+#print(data2.files)
+for keys in data2.files:
+    print(keys)
+#print(data['world_mat_0'])
 #print(data['camera_mat_inv_0'])
-print(data['scale_mat_0'])
+#print(data['scale_mat_0'])
